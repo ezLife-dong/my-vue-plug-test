96b8bf92c610b24bab2826c36207dd523073fd56
董凯 <dongkai01@meicai.cn>
2018-06-27 15:02:45 +0800

    vue-plug-1st

---
+++ src/app.vue
@@ -0,0 +1,30 @@
+<style lang="less" scoped>
+    @import url('styles/common.less');
+</style>
+<template>
+    <div class="msg-wrapper">
+        <span>hello!</span>
+        <span class="account">{{data.name}}</span>
+    </div>
+</template>
+<script>
+export default {
+    name:'Msg',
+    data () {
+        return {}
+    },
+    props:{
+        data:{
+            type:Object
+        }
+    },
+    methods:{
+
+    },
+    created(){
+        let that = this;
+    }
+}
+</script>
+
+
